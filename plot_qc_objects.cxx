7592f3e9b31af81207dbe8a8c7485dc96a3ea5ad
David <david.grund@cern.ch>
2024-06-02 12:54:11 +0200

    Change path to plots

--- plot_qc_objects.cxx
+++ plot_qc_objects.cxx
@@ -11,7 +11,8 @@
 bool create_plots (run_map rm, configuration cfg, vector<histogram> hlst, bool debug)
 {
   cout << "\n";
-  string path = Form("plots/%s/", cfg.get_group().data());
+  string path = Form("%s%s/", PLOTS_FOLDER.data(), cfg.get_group().data());
+  gSystem->Exec(Form("mkdir -p %s", path.data()));
 
   // compare runs
   if(cfg.get_compare() == "runs") 
@@ -24,12 +25,13 @@ bool create_plots (run_map rm, configuration cfg, vector<histogram> hlst, bool d
     int n_passes = cfg.get_n_passes();
     int n_per_round = (n_colors * n_styles);
     cout << "Plotting comparison of runs. Total rounds: " << n_rounds << ", passes: " << n_passes << "\n"
+         << " Folder: " << path << "\n"
          << " Reference run: " << ref.run << ", " << ref.period << ", " << ref.pass << "\n";
     for (int r = 0; r < n_rounds; r++) 
     {
       cout << "\n Round " << r+1 << ":\n";
-      if(n_rounds > 1) path = Form("plots/%s_%02i/", cfg.get_group().data(), r+1);
-      gSystem->Exec(Form("mkdir -p %s", path.data()));
+      string prefix = "";
+      if(n_rounds > 1) prefix = Form("%02i_", r+1);
 
       int first = r * n_per_round;
       int last = first;
@@ -42,15 +44,13 @@ bool create_plots (run_map rm, configuration cfg, vector<histogram> hlst, bool d
         string name = h.name_short;
         //cout << "  QC object: " << name << "\n";
         ratio_plot rplt;
-        rplt.create_plot(cfg, rm, path, "", h, ref, list_part, debug);
+        rplt.create_plot(cfg, rm, path, prefix, h, ref, list_part, debug);
       }
     }
   } 
   // compare passes
   else if(cfg.get_compare() == "passes") 
   {
-    gSystem->Exec(Form("mkdir -p %s", path.data()));
-
     vector<int> list_runs = cfg.get_run_list();
     vector<string> list_pass = cfg.get_pass_list();
     vector<string> list_per_mc = cfg.get_period_mc_list();
